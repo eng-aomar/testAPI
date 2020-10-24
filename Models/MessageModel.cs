388faa5bcb2a5a0dce477ceb1ade5c995d6fd08b
Alaa Omar <alaa.omer2009@gmail.com>
2020-10-24 15:49:28 +0300

    Add project files.

---
+++ Models/MessageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class MessageModel
+    {
+        public string id { get; set; }
+
+        public string messageText { get; set; }
+
+        public DateTime createdAT { get; set; }
+        public DateTime updatedAT { get; set; }
+    }
+}
